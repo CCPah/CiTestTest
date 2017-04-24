acc77f2a87bcc6d6193d86c110d06d149934090f
Patrick Alexander Hill <pahill@mail.uni-paderborn.de>
2017-04-24 06:08:01 +0200

    code & stuff

---
+++ src/SampleTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void test1() {
+		fail("Just a Test");
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue("Just another Test", true);
+	}
+
+}
