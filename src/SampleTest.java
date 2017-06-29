a66c824147ca8a0c262ce0432d136f2ba3748578
Patrick Alexander Hill <pahill@mail.uni-paderborn.de>
2017-06-29 17:58:00 +0200

    changes

--- src/SampleTest.java
+++ src/SampleTest.java
@@ -5,11 +5,6 @@ import org.junit.Test;
 public class SampleTest {
 	
 	@Test
-	public void test1() {
-		assertTrue("Just a Test", true);
-	}
-	
-	@Test
 	public void test2() {
 		assertTrue("Just another Test", true);
 	}
